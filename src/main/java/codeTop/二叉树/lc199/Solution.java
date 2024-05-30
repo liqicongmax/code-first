dce3fb7908d713f0c0d47d107032f46014f94e3e
liqicongmax <952267787@qq.com>
2024-05-30 23:42:45 +0800

    init 20240530 23:42 3 tree

---
+++ src/main/java/codeTop/二叉树/lc199/Solution.java
@@ -0,0 +1,82 @@
+package codeTop.二叉树.lc199;//给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//
+// 
+//输入: [1,2,3,null,5,null,4]
+//输出: [1,3,4]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: [1,null,3]
+//输出: [1,3]
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: []
+//输出: []
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 二叉树的节点个数的范围是 [0,100] 
+// 
+// -100 <= Node.val <= 100 
+// 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1071 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import codeTop.base.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(root,0,res);
+        return res;
+    }
+
+
+    public void dfs(TreeNode root,int depth,List<Integer> res){
+        if(root==null){
+            return;
+        }
+        if(depth==res.size()){
+            res.add(root.val);
+        }
+        dfs(root.right,depth+1,res);
+        dfs(root.left,depth+1,res);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
